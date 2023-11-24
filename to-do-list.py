d34a56bc2842f5d17c85f7528afaeabd4e868e17
Mohammed Nihal <nihalcrixcrox56@gmail.com>
2023-11-24 19:03:55 +0530

    updted version

--- to-do-list.py
+++ to-do-list.py
@@ -1,6 +1,8 @@
 book=[]
 def insert_tasks():
-    book.append(input('input Task: '))
+    task=input('input Task: ')
+    book.append(task)
+
 
 def delete_tasks():
     try:
@@ -9,23 +11,30 @@ def delete_tasks():
     except IndexError:
         print('there is no Item to Delete')
 def display_tasks():
-    print(book)        
+    if not book:
+        print('No tasks to display')
+    else:
+        print('---------------------To-Do-List------------------------')
+        for i, task in enumerate(book):
+            print(f"{i}. {task}")
+
 i=0
 print('---------------------To-Do-List------------------------')
 while True:
-    print('Operations:')
-    
-    ch=int(input('\n1.Insert\n2.Delete\n3.Display\n4.Exit\n>>'))
-    if ch==1:
-        insert_tasks()
-    elif ch==2:
-        delete_tasks()
-    elif ch==3:
-        display_tasks()
-    elif ch==4:
-        break
-    elif ch>4:
-        print(f'{ch} is a Wrong Choice\n choose 1,2,3 or 4')
-        
+    print('\n\nOperations:')
+    try:
+        ch=int(input('\n1.Insert\n2.Delete\n3.Display\n4.Exit\n>>'))
+        if ch==1:
+            insert_tasks()
+        elif ch==2:
+            delete_tasks()
+        elif ch==3:
+            display_tasks()
+        elif ch==4:
+            break
+        elif ch>4:
+            print(f'{ch} is a Wrong Choice\n choose 1,2,3 or 4')
+    except ValueError:
+        print(f'{ch} is an Inavlid Input')
 
         
